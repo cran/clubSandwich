16042f12bf0500b27cb9231bbde525d3757dc85a
James Pustejovsky <jepusto@gmail.com>
2020-04-17 07:30:02 +0000

    version 0.4.2

--- inst/doc/panel-data-CRVE.R
+++ inst/doc/panel-data-CRVE.R
@@ -1,4 +1,4 @@
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 library(clubSandwich)
 data(MortalityRates)
 
@@ -10,22 +10,22 @@ MV_deaths <- subset(MortalityRates, cause=="Motor Vehicle" &
 lm_unweighted <- lm(mrate ~ 0 + legal + beertaxa + 
                       factor(state) + factor(year), data = MV_deaths)
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 coef_test(lm_unweighted, vcov = "CR1", 
           cluster = MV_deaths$state, test = "naive-t")[1:2,]
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 coef_test(lm_unweighted, vcov = "CR2", 
           cluster = MV_deaths$state, test = "Satterthwaite")[1:2,]
 
-## ---- message = FALSE----------------------------------------------------
+## ---- message = FALSE-----------------------------------------------------------------------------
 library(plm)
 plm_unweighted <- plm(mrate ~ legal + beertaxa, data = MV_deaths, 
                       effect = "twoways", index = c("state","year"))
 coef_test(plm_unweighted, vcov = "CR1", cluster = "individual", test = "naive-t")
 coef_test(plm_unweighted, vcov = "CR2", cluster = "individual", test = "Satterthwaite")
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 lm_weighted <- lm(mrate ~ 0 + legal + beertaxa + factor(state) + factor(year), 
                   weights = pop, data = MV_deaths)
 coef_test(lm_weighted, vcov = "CR1", 
@@ -33,19 +33,19 @@ coef_test(lm_weighted, vcov = "CR1",
 coef_test(lm_weighted, vcov = "CR2", 
           cluster = MV_deaths$state, test = "Satterthwaite")[1:2,]
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 coef_test(lm_weighted, vcov = "CR2", 
           cluster = MV_deaths$state, target = 1 / MV_deaths$pop, 
           test = "Satterthwaite")[1:2,]
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 plm_random <- plm(mrate ~ 0 + legal + beertaxa + year, data = MV_deaths, 
                   effect = "individual", index = c("state","year"),
                   model = "random")
 coef_test(plm_random, vcov = "CR1", test = "naive-t")[1:2,]
 coef_test(plm_random, vcov = "CR2", test = "Satterthwaite")[1:2,]
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 MV_deaths <- within(MV_deaths, {
   legal_cent <- legal - tapply(legal, state, mean)[factor(state)]
   beer_cent <- beertaxa - tapply(beertaxa, state, mean)[factor(state)]
@@ -57,10 +57,10 @@ plm_Hausman <- plm(mrate ~ 0 + legal + beertaxa + legal_cent + beer_cent + facto
                    model = "random")
 coef_test(plm_Hausman, vcov = "CR2", test = "Satterthwaite")[1:4,]
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 Wald_test(plm_Hausman, constraints = c("legal_cent","beer_cent"), 
           vcov = "CR1", test = "chi-sq")
 
-## ------------------------------------------------------------------------
+## -------------------------------------------------------------------------------------------------
 Wald_test(plm_Hausman, constraints = c("legal_cent","beer_cent"), vcov = "CR2")
 
